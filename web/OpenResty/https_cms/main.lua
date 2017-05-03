9ee41ab1de86b144b18339ea86be98a1ffdf6945
luoweis <luoweis@126.com>
2017-05-03 08:41:59 +0800

    2017-5-3

--- web/OpenResty/https_cms/main.lua
+++
@@ -1,1 +0,0 @@
-ngx.say("<html><head><meta http-equiv='content-type' content='text/html;charset=utf-8'></head><title>Tszins</title><body><h1>您现在访问的是cms.tszins.tv的测试页面</h1><h2>增加了https协议</h2></body></html>")
